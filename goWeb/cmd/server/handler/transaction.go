d81e48a48db287fbb0470fe9c6010f9b9a7bd300
juanimeli <juanmartin.guglielmone@mercadolibre.com>
2022-10-06 12:09:02 -0300

    C3-GoWeb-TM: request validation and response management implemented

--- goWeb/cmd/server/handler/transaction.go
+++ goWeb/cmd/server/handler/transaction.go
@@ -15,10 +15,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/juanimeli/backpack-bcgow6-juan-guglielmone/goWeb/internal/transactions"
-)
-
-const (
-	filePath = "./transactions.json"
+	"github.com/juanimeli/backpack-bcgow6-juan-guglielmone/goWeb/pkg/web"
 )
 
 type request struct { //la estructura request es igual
@@ -50,22 +47,22 @@ func (c *Transaction) Store() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(401, gin.H{"error": "invalid token"})
+			ctx.JSON(401, web.NewResponse(401, nil, "invalid token"))
 			return
 		}
 
 		var r request
 		if err := ctx.ShouldBindJSON(&r); err != nil {
-			ctx.JSON(404, gin.H{"error": fmt.Errorf("field %s is required", err.Error())})
+			ctx.JSON(404, web.NewResponse(401, nil, fmt.Sprintf("field %s is required", err.Error())))
 			return
 		}
 
 		t, err := c.service.Store(r.Codigo, r.Moneda, r.Monto, r.Emisor, r.Receptor, r.Fecha)
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(401, web.NewResponse(401, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, t)
+		ctx.JSON(200, web.NewResponse(200, t, ""))
 	}
 
 }
@@ -74,11 +71,9 @@ func (c *Transaction) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		t, err := c.service.GetAll()
 		if err != nil {
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(400, web.NewResponse(400, nil, err.Error()))
 		}
-		ctx.JSON(200, gin.H{"content": t})
+		ctx.JSON(200, web.NewResponse(200, t, ""))
 	}
 
 }
@@ -86,50 +81,50 @@ func (c *Transaction) Update() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(401, gin.H{"error": "invalid token"})
+			ctx.JSON(401, web.NewResponse(401, nil, "invalid token"))
 			return
 		}
 		id, err := strconv.ParseInt(ctx.Param("ID"), 10, 64)
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, "invalid ID"))
 			return
 		}
 		var r request
 
 		if err := ctx.ShouldBindJSON(&r); err != nil {
-			ctx.JSON(400, gin.H{"error": err.Error()})
+			ctx.JSON(400, web.NewResponse(400, nil, err.Error()))
 			return
 		}
 		if r.Codigo == "" {
-			ctx.JSON(400, gin.H{"error": "cod is required"})
+			ctx.JSON(400, web.NewResponse(400, nil, "cod is required"))
 			return
 		}
 		if r.Moneda == "" {
-			ctx.JSON(400, gin.H{"error": "currency is required"})
+			ctx.JSON(400, web.NewResponse(400, nil, "currency is required"))
 			return
 		}
-		if r.Monto < 0 {
-			ctx.JSON(400, gin.H{"error": "amount cant be negative"})
+		if r.Monto == 0 {
+			ctx.JSON(400, web.NewResponse(400, nil, "amount is required"))
 			return
 		}
 		if r.Emisor == "" {
-			ctx.JSON(400, gin.H{"error": "sender is required"})
+			ctx.JSON(400, web.NewResponse(400, nil, "sender is required"))
 			return
 		}
 		if r.Receptor == "" {
-			ctx.JSON(400, gin.H{"error": "receiver is required"})
+			ctx.JSON(400, web.NewResponse(400, nil, "receiver is required"))
 			return
 		}
 		if r.Fecha == "" {
-			ctx.JSON(400, gin.H{"error": "date is required"})
+			ctx.JSON(400, web.NewResponse(400, nil, "date is required"))
 			return
 		}
 		t, err := c.service.Update(int(id), r.Codigo, r.Moneda, r.Monto, r.Emisor, r.Receptor, r.Fecha)
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(400, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, t)
+		ctx.JSON(200, web.NewResponse(200, t, ""))
 	}
 }
 
@@ -137,20 +132,20 @@ func (c *Transaction) Delete() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(401, gin.H{"error": "invalid token"})
+			ctx.JSON(401, web.NewResponse(401, nil, "invalid token"))
 			return
 		}
 		id, err := strconv.ParseInt(ctx.Param("ID"), 10, 64)
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, "invalid ID"))
 			return
 		}
 		err = c.service.Delete(int(id))
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(401, web.NewResponse(401, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, gin.H{"data": fmt.Sprintf("transaction with id %d has been deleted", id)})
+		ctx.JSON(200, web.NewResponse(200, fmt.Sprintf("transaction with id %d has been deleted", id), ""))
 
 	}
 }
@@ -158,33 +153,33 @@ func (c *Transaction) UpdateCodnAmount() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			ctx.JSON(401, gin.H{"error": "invalid token"})
+			ctx.JSON(401, web.NewResponse(401, nil, "invalid token"))
 			return
 		}
 		id, err := strconv.ParseInt(ctx.Param("ID"), 10, 64)
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, "invalid ID"))
 			return
 		}
 		var r request
 		if err := ctx.ShouldBindJSON(&r); err != nil {
-			ctx.JSON(405, gin.H{"error": err.Error()})
+			ctx.JSON(400, web.NewResponse(400, nil, err.Error()))
 			return
 		}
 		if r.Codigo == "" {
-			ctx.JSON(400, gin.H{"error": "transaction cod is required"})
+			ctx.JSON(400, web.NewResponse(400, nil, "transaction cod is required"))
 			return
 		}
-		if r.Monto < 0 {
-			ctx.JSON(400, gin.H{"error": "transaction amount must be grater or equal 0"})
+		if r.Monto == 0 {
+			ctx.JSON(400, web.NewResponse(400, nil, "transaction amount is required"))
 		}
 
 		t, err := c.service.UpdateCodnAmount(int(id), r.Codigo, r.Monto)
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, err.Error()))
 			return
 		}
-		ctx.JSON(200, t)
+		ctx.JSON(200, web.NewResponse(200, t, ""))
 	}
 }
 
--- goWeb/cmd/server/main.go
+++ goWeb/cmd/server/main.go
@@ -128,6 +128,23 @@ pasos:
 
 1. En lugar de leer los valores de nuestra entidad en memoria, se debe obtener del
 archivo generado en el punto anterior.
+
+Clase 4
+TM - Ejercicio 1 - Manejo de respuestas genéricas
+Se requiere implementar un manejo de respuestas genéricas para enviar siempre el mismo formato
+en las peticiones. Para lograrlo se deben realizar los siguientes pasos:
+Generar el paquete web dentro del directorio pkg.
+Realizar la estructura Response con los capos: code, data y error:
+code tendra el codigo de retorno.
+data tendrá la estructura que envía la aplicación (en caso que no haya error).
+error tendrá el error recibido en formato texto (en caso que haya error).
+Desarrollar una función que reciba el code cómo entero, data como interfaz y error como string.
+La función debe retornar en base al código, si es una respuesta con el data o con el error.
+Implementar esta función en todos los retornos de los controladores, antes de enviar la respuesta
+al cliente la función debe generar la estructura que definimos.
+
+
+
 */
 
 import (
@@ -140,6 +157,10 @@ import (
 	"github.com/juanimeli/backpack-bcgow6-juan-guglielmone/goWeb/pkg/store"
 )
 
+const (
+	dbFilePath = "./transactions.json"
+)
+
 func main() {
 
 	err := godotenv.Load()
@@ -149,7 +170,7 @@ func main() {
 
 	//fmt.Println(ReadJson(filePath))
 
-	db := store.New(store.FileType, "./transactions.json")
+	db := store.New(store.FileType, dbFilePath)
 	repo := transactions.NewRepository(db)
 	service := transactions.NewService(repo)
 
