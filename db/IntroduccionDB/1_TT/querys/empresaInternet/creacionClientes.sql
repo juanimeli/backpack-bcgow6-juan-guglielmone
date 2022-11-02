fb662c1e6c22abde33f1ca6c27b1849b06d30fee
juanimeli <juanmartin.guglielmone@mercadolibre.com>
2022-11-02 09:39:49 -0300

    IntroduccionDB-db-TT: tables created and answers for empresaInternet

---
+++ db/IntroduccionDB/1_TT/querys/empresaInternet/creacionClientes.sql
@@ -0,0 +1,10 @@
+INSERT INTO cliente VALUES (12345, "Juan", "Perez", "1994-10-2", "Buenos Aires", "Buenos Aires");
+INSERT INTO cliente VALUES (23456, "Maria", "Gonzalez", "1990-10-2", "Montevideo", "Montevideo");
+INSERT INTO cliente VALUES (1234425, "Camila", "Mendez", "2000-10-2", "Canelones", "Canelones");
+INSERT INTO cliente VALUES (123445, "Magdalena", "Martinez", "1980-10-2", "Artigas", "Artigas");
+INSERT INTO cliente VALUES (123645, "Ana", "Sosa", "1960-10-2", "Rocha", "Rocha");
+INSERT INTO cliente VALUES (12345123, "Pablo", "Rodriguez", "1978-10-2", "Buenos Aires", "Buenos Aires");
+INSERT INTO cliente VALUES (1232145, "Ricardo", "Perez", "1994-10-2", "Buenos Aires", "Buenos Aires");
+INSERT INTO cliente VALUES (1234445, "Jose", "Perez", "1994-10-2", "Buenos Aires", "Buenos Aires");
+INSERT INTO cliente VALUES (123345, "Luis", "Perez", "1994-10-2", "Buenos Aires", "Buenos Aires");
+INSERT INTO cliente VALUES (1231145, "Andres", "Perez", "1994-10-2", "Buenos Aires", "Buenos Aires");
